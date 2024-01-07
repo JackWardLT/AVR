0113bcd00b262bc9f23a9aa362976d4c4457320b
JackWardLT <larsntnu@gmail.com>
2024-01-07 16:11:04 +0100

    Setup

---
+++ LeCode/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 07.01.2024 16:09:29
+ * Author : Lars
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
