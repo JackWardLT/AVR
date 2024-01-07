8265a2e171e7d136a49ffcea0bca91f97ded3f54
JackWardLT <larsntnu@gmail.com>
2024-01-07 17:07:37 +0100

    Lecture 3 Assignment

--- LeCode/GccApplication1/GccApplication1/main.c
+++ LeCode/GccApplication1/GccApplication1/main.c
@@ -6,13 +6,32 @@
  */ 
 
 #include <avr/io.h>
+#define F_CPU 1000000UL  // Adjust as per your clock
+#include <util/delay.h>
 
+void f(){
+	PORTD  |= (1 << PD6);
+	_delay_ms(500);
+	PORTD &= ~(1 << PD6);
+	_delay_ms(500);
+}
+
+void s(){
+	PORTD  |= (1 << PD6);
+	_delay_ms(2000);
+	PORTD &= ~(1 << PD6);
+	_delay_ms(2000);	
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+	DDRB &= ~(1 << PB0);
+	DDRD |= (1 << PD6); 
+	PORTD |= (1 << PB0);
 
+	while (1)
+	{
+		if (bit_is_clear(PINB, PB0)) f();
+		else s();
+	}
+}
