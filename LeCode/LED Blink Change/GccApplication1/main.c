c440bca17e05d29ea63ecac84df8c524627955eb
JackWardLT <larsntnu@gmail.com>
2024-01-07 18:28:49 +0100

    Task2

--- LeCode/LED Blink Change/GccApplication1/main.c
+++ LeCode/LED Blink Change/GccApplication1/main.c
@@ -8,30 +8,72 @@
 #include <avr/io.h>
 #define F_CPU 1000000UL  // Adjust as per your clock
 #include <util/delay.h>
+#define LEDPIN PD4
+#define OLP PD4
+#define ILP PB1
+
+void f();
+void s();
+void Task1();
+void Task2();
+
+int BP = 0;
+
+
+
+
+
+int main(void)
+{
+
+
+	while (1)
+	{
+		//Task1();
+		Task2();
+	}
+}
+
+void Task2(){
+	
+	if(bit_is_clear(PINB, ILP)){
+		BP++;
+		if (BP == 1)
+		{
+			s();
+		}
+		if (BP == 2)
+		{
+			f();
+		}
+		else BP = 0;
+	}
+}
+
+
+/************************************************************************/
+/*	Task 1 | Lecture 3                                                  */
+/************************************************************************/
+
+void Task1(){
+	DDRB &= ~(1 << PB1);
+	DDRD |= (1 << LEDPIN);
+	PORTD |= (1 << PB1);
+	
+	if (bit_is_clear(PINB, PB1)) f();
+	else s();	
+}
 
 void f(){
-	PORTD  |= (1 << PD6);
+	PORTD  |= (1 << LEDPIN);
 	_delay_ms(500);
-	PORTD &= ~(1 << PD6);
+	PORTD &= ~(1 << LEDPIN);
 	_delay_ms(500);
 }
 
 void s(){
-	PORTD  |= (1 << PD6);
+	PORTD  |= (1 << LEDPIN);
 	_delay_ms(2000);
-	PORTD &= ~(1 << PD6);
-	_delay_ms(2000);	
-}
-
-int main(void)
-{
-	DDRB &= ~(1 << PB0);
-	DDRD |= (1 << PD6); 
-	PORTD |= (1 << PB0);
-
-	while (1)
-	{
-		if (bit_is_clear(PINB, PB0)) f();
-		else s();
-	}
+	PORTD &= ~(1 << LEDPIN);
+	_delay_ms(2000);
 }
